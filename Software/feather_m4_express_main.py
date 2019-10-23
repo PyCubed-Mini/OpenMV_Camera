7ae270217afe9c0b1da5bc0692bd6e1473e5db65
ashtofen <48425721+ashtofen@users.noreply.github.com>
2019-10-22 23:47:58 -0700

    Enabled Multi-SPI Comms; Cleaned Up Code
    Modified SPI software to accommodate overhaul to hardware interfacing.
    The MCU can now accommodate both the camera and an SD card breakout board.
    
    Remember to rename this file to main.py if you're using it on an actual Feather M4 Express.

--- Software/feather_m4_express_main.py
+++ Software/feather_m4_express_main.py
@@ -1,24 +1,50 @@
-# CircuitPython demo - NeoPixel
+'''
+### Feather M4 Express Main Module
+## Allan Shtofenmakher
+# Spacecraft Design Lab
+
+# AKA main.py
+# Reads SD card file names and communicates with OpenMV camera 
+# and SD card breakout board over SPI
+# Features NeoPixel rainbow_cycle to indicate proper operation
+
+# Project Start Date: 21 October 2019
+# Last Updated: 22 October 2019
+
+'''
+
+
+# import relevant libraries
+import os
+import sys
 import time
 import board
 import busio
+import storage
 import neopixel
+import adafruit_sdcard
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_bus_device.spi_device import SPIDevice
 
+# set up NeoPixel rainbow_cycle
 pixel_pin = board.NEOPIXEL
 num_pixels = 1
-
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, auto_write=False)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.05, auto_write=False)
 
 # set up SPI communication
 spi = busio.SPI(board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-cs_cam = DigitalInOut(board.D4)  # USE PIN 4
-# cs.direction = Direction.OUTPUT
-# cs.value = True
+cs_cam = DigitalInOut(board.D4)  # use pin 4 for camera
+cs_sd = DigitalInOut(board.D5)  # use pin 5 for SD card
 camera = SPIDevice(spi, cs_cam, baudrate=115200, polarity=0, phase=0)
+sdcard = adafruit_sdcard.SDCard(spi, cs_sd)
+
+# Use the filesystem as normal! Our files are under /sd
+vfs = storage.VfsFat(sdcard)
+storage.mount(vfs, "/sd")
+sys.path.append("/sd")
 
 
+# define helper function wheel() to improve NeoPixel performance
 def wheel(pos):
     # Input a value 0 to 255 to get a color value.
     # The colours are a transition r - g - b - back to r.
@@ -33,6 +59,7 @@ def wheel(pos):
     return (pos * 3, 0, 255 - pos * 3)
 
 
+# define helper function color_chase() to improve NeoPixel performance
 def color_chase(color, wait):
     for i in range(num_pixels):
         pixels[i] = color
@@ -41,6 +68,7 @@ def color_chase(color, wait):
     time.sleep(0.5)
 
 
+# define helper function rainbow_cycle() to improve NeoPixel performance
 def rainbow_cycle(wait):
     for j in range(255):
         for i in range(num_pixels):
@@ -50,15 +78,56 @@ def rainbow_cycle(wait):
         time.sleep(wait)
 
 
-RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
+# define helper function to print the contents of the SD
+def print_directory(path, tabs=0):
+    for file in os.listdir(path):
+        try:
+            stats = os.stat(path + "/" + file)  # changed from statvfs()
+            filesize = stats[6]
+            isdir = stats[0] & 0x4000
+     
+            if filesize < 1000:
+                sizestr = str(filesize) + " by"
+            elif filesize < 1000000:
+                sizestr = "%0.1f KB" % (filesize / 1000)
+            else:
+                sizestr = "%0.1f MB" % (filesize / 1000000)
+     
+            prettyprintname = ""
+            for _ in range(tabs):
+                prettyprintname += "   "
+            prettyprintname += file
+            if isdir:
+                prettyprintname += "/"
+            print('{0:<40} Size: {1:>10}'.format(prettyprintname, sizestr))
+     
+            #recursively print directory contents
+            if isdir:
+                print_directory(path + "/" + file, tabs + 1)
+        except:
+            print('error')
+            pass
+ 
 
+# print SD card file names and stats
+print('Files on filesystem:')
+print('--------------------')
+print_directory("/sd")
+
+# enable this to see more lights:
+# RED = (255, 0, 0)
+# YELLOW = (255, 150, 0)
+# GREEN = (0, 255, 0)
+# CYAN = (0, 255, 255)
+# BLUE = (0, 0, 255)
+# PURPLE = (180, 0, 255)
+
+# initialize loop index i
 i = 0
+
 while True:
+    
+    # enable this to see more lights:
     # pixels.fill(RED)
     # pixels.show()
     # # Increase or decrease to change the speed of the solid color change.
@@ -70,6 +139,7 @@ while True:
     # pixels.show()
     # time.sleep(1)
 
+    # enable this to see more lights:
     # color_chase(RED, 0.1)  # Increase the number to slow down the color chase
     # color_chase(YELLOW, 0.1)
     # color_chase(GREEN, 0.1)
@@ -77,23 +147,30 @@ while True:
     # color_chase(BLUE, 0.1)
     # color_chase(PURPLE, 0.1)
     
+	# increment loop_index, divide by 10, and redefine loop index as remainder
     i = (i+1) % 10  # run from 0 to 9
     
+	# run SPI communication once every ten loops
     if (i % 10) == 0:
     
-      with camera:
-          # spi.write(b'hello')
+	  # communicate with camera over SPI
+      with camera as spi:
+          
+		  # prepare buffer
           buf = bytearray(5)
+          spi.write_readinto(b'hello', buf)
           # spi.write(b'hello')  # for writing only
           # spi.readinto(buf)  # for reading only
-          spi.write_readinto(b'hello', buf)
           
           # strange workaround to enable printing bytearray buf
-          # print(buf)
           print(''.join(chr(b) for b in buf))
           
           # slow down the process
           time.sleep(0.1)
+      
+      
+      # FOR REFERENCE ONLY: 
+      # old-fashioned SPI code
       
       # while not spi.try_lock():
       #     pass
@@ -108,4 +185,4 @@ while True:
 
     rainbow_cycle(0)  # Increase the number to slow down the rainbow
     
-    time.sleep(0.01) # make bigger to slow down
+    # time.sleep(0.01) # uncomment and make bigger to slow down
